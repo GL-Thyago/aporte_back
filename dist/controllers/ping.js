bd9a81d990c686f3fd986e979cec13f7f8370d4c
Raul Oliveira <134628269+GL-Thyago@users.noreply.github.com>
2025-10-17 12:41:58 -0300

    atualizar banco4

--- dist/controllers/ping.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ping = void 0;
-const ping = async () => {
-    console.log(`Ping recebido para a rodada `);
-    return `Ping recebido para a rodada `;
-};
-exports.ping = ping;
